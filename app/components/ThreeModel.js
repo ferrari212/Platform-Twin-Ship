6b422d527a34a70d2dd977fd51e832642d044e86
ferrari212 <felipe.ferrari.212@gmail.com>
2020-12-24 09:29:40 +0100

    dev 24/12/2020

--- app/components/ThreeModel.js
+++ app/components/ThreeModel.js
@@ -19,6 +19,11 @@ class ThreeModel extends Component {
 	constructor(props) {
 		super(props)
 
+		// this.props = props
+		console.log(this)
+
+		this.addScenario = this.props.addScenario
+
 		console.log("Constructor")
 	}
 
@@ -26,15 +31,15 @@ class ThreeModel extends Component {
 		// Globals
 		this.oSize = 2048
 		this.ship = new Vessel.Ship(GunnerusTeste)
-		// this.setState({ ship: this.ship })
+		// console.log(GunnerusTeste)
 
 		const ourRequest = Axios.CancelToken.source()
 
 		async function fetchPosts(componet) {
 			try {
 				const response = await Axios.get(`/profile/${componet.props.user.username}/posts`, { cancelToken: ourRequest.token })
-				console.log(response.data[3])
-				componet.setState({ newShip: response.data[3] })
+				console.log(response.data[0])
+				await componet.setState({ newShip: JSON.parse(response.data[0].ship) })
 			} catch (e) {
 				console.log("There was a problem.", e)
 			}
@@ -42,7 +47,7 @@ class ThreeModel extends Component {
 		fetchPosts(this)
 
 		this.sceneSetup()
-		this.addCustomSceneObjects()
+		// this.addScenario()
 		this.startAnimationLoop()
 
 		window.addEventListener("resize", this.handleWindowResize)
@@ -53,6 +58,10 @@ class ThreeModel extends Component {
 	componentDidUpdate(prevProps, prevStates) {
 		console.log("Component did Update!", prevProps, prevStates)
 		console.log(this.state)
+		console.log(this.state.newShip)
+		// this.ship = new Vessel.Ship(this.state.newShip)
+		// this.addScenario()
+		this.addShip()
 	}
 
 	sceneSetup = () => {
@@ -65,8 +74,8 @@ class ThreeModel extends Component {
 		this.camera = new THREE.PerspectiveCamera(
 			75, // fov = field of view
 			width / height, // aspect ratio
-			1, // near plane
-			1000 // far plane
+			0.01, // near plane
+			2000 // far plane
 		)
 
 		// set some distance from a cube that is located at z = 0
@@ -78,29 +87,12 @@ class ThreeModel extends Component {
 		this.mount.appendChild(this.renderer.domElement) // mount using React ref
 
 		this.useZUp = () => {
-			// THREE.Object3D.DefaultUp.set(0, 0, 1)
 			const zUpCont = new THREE.Group()
 			this.scene.add(zUpCont)
-
-			// this.camera.up.set(0, 1, 0)
-			// this.camera.lookAt(zUpCont.position)
-
-			// Maybe use zUpCont
-			// return zUpCont
 		}
 	}
 
-	addCustomSceneObjects = () => {
-		// const geometry = new THREE.BoxGeometry(2, 2, 2)
-		// const material = new THREE.MeshPhongMaterial({
-		// 	color: 0x156289,
-		// 	emissive: 0x072534,
-		// 	side: THREE.DoubleSide,
-		// 	flatShading: true
-		// })
-		// this.cube = new THREE.Mesh(geometry, material)
-		// this.scene.add(this.cube)
-
+	addScenario = () => {
 		const sun = new THREE.DirectionalLight(0xffffff, 2)
 		sun.position.set(-512, -246, 128)
 		this.scene.add(sun)
@@ -119,7 +111,14 @@ class ThreeModel extends Component {
 		})
 		this.ocean.name = "Ocean"
 		this.scene.add(this.ocean)
-		this.scene.rotation.x = -Math.PI / 2
+	}
+
+	addShip = () => {
+		this.scene.background = new THREE.Color(0xa9cce3)
+		const ambientLight = new THREE.AmbientLight(0xffffff, 0.3)
+		const mainLight = new THREE.DirectionalLight(0xffffff, 1)
+		mainLight.position.set(1, 1, 1)
+		this.scene.add(ambientLight, mainLight)
 
 		this.ship3D = new Ship3D(this.ship, {
 			// stlPath: "specs/STL files",
@@ -136,36 +135,11 @@ class ThreeModel extends Component {
 
 		this.scene.add(this.ship3D)
 
-		// Vessel.loadShip(GunnerusTeste, function (ship) {
-		// 	this.ship3D = new Ship3D(ship, {
-		// 		// stlPath: "specs/STL files",
-		// 		stlPath: "specs/STL files/Gunnerus",
-		// 		upperColor: 0x33aa33,
-		// 		lowerColor: 0xaa3333,
-		// 		hullOpacity: 1,
-		// 		deckOpacity: 1,
-		// 		objectOpacity: 1
-		// 	})
-		// 	this.ship3D.name = "Ship3D"
-		// 	this.ship3D.show = "on"
-		// 	this.scene.add(ship3D)
-
-		// 	// Take the hydrostatics informations
-		// 	var ship = this.ship3D.ship
-		// 	var draftDesign = ship.designState.calculationParameters.Draft_design
-		// 	var hull = ship.structure.hull
-
-		// 	// hydrostatics = hull.calculateAttributesAtDraft(draftDesign)
-		// 	// ship3D.shipState
-		// 	// ship3D.ship.calculateStability()
-		// 	// Maybe take KMt or KMl to the vessel.js
-		// 	// hydrostatics.KMt = hydrostatics.BMt + hydrostatics.KB
-		// 	// hydrostatics.KMl = hydrostatics.BMl + hydrostatics.KB
-		// })
+		this.scene.rotation.x = -Math.PI / 2
 	}
 
 	startAnimationLoop = () => {
-		this.ocean.water.material.uniforms.time.value += 1 / 60
+		// this.ocean.water.material.uniforms.time.value += 1 / 60
 
 		this.renderer.render(this.scene, this.camera)
 		this.requestID = window.requestAnimationFrame(this.startAnimationLoop)
