96902ce08012b46aac150512ccd29efbb83b092c
ferrari212 <felipe.ferrari.212@gmail.com>
2021-01-17 22:17:15 +0100

    dev 17/01/2020

--- app/components/GraphicVega.js
+++ app/components/GraphicVega.js
@@ -1,35 +1,30 @@
 import React, { useEffect } from "react"
+import BarChart from "./BarChart.js"
 import ReactDOM from "react-dom"
-import { Vega } from "react-vega/esm"
+import { Vega } from "react-vega"
+import { createClassFromSpec } from "react-vega"
 
 function GraphicVega() {
-	const spec = {
-		width: 400,
-		height: 200,
-		data: [{ name: "table" }],
-		signals: [
-			{
-				name: "tooltip",
-				value: {},
-				on: [
-					{ events: "rect:mouseover", update: "datum" },
-					{ events: "rect:mouseout", update: "{}" }
-				]
-			}
-		]
-	}
-
 	const barData = {
-		table: []
+		table: [
+			{ category: "A", amount: 28 },
+			{ category: "B", amount: 55 },
+			{ category: "C", amount: 43 },
+			{ category: "D", amount: 91 },
+			{ category: "E", amount: 81 },
+			{ category: "F", amount: 53 },
+			{ category: "G", amount: 19 },
+			{ category: "H", amount: 89 }
+		]
 	}
 
 	function handleHover(...args) {
 		console.log(args)
 	}
-
 	const signalListeners = { hover: handleHover }
 
-	return <Vega spec={spec} data={barData} signalListeners={signalListeners} />
+	// return <Vega spec={spec} data={barData} signalListeners={signalListeners} />
+	return <BarChart data={barData} signalListeners={signalListeners} />
 }
 
 export default GraphicVega
